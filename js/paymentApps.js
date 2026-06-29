1bfa1eea9b282734db6f75d7d6216952887de90f
Ch lithin sai kumar <lithinsaikumar05@gmail.com>
2026-06-30 00:19:05 +0530

    Update paymentApps.js

--- js/paymentApps.js
+++ js/paymentApps.js
@@ -1,32 +1,15 @@
-// js/paymentApps.js
-
 function getPaymentLinks(upiLink) {
 
     const query = upiLink.replace("upi://pay?", "");
 
     return {
+
         bhim: "bhim://pay?" + query,
+
         phonepe: "phonepe://pay?" + query,
+
         any: upiLink
+
     };
-}
-
-function launchPayment(app, upiLink){
-
-    const links = getPaymentLinks(upiLink);
-
-    switch(app){
-
-        case "bhim":
-            window.location.href = links.bhim;
-            break;
-
-        case "phonepe":
-            window.location.href = links.phonepe;
-            break;
-
-        default:
-            window.location.href = links.any;
-    }
 
 }
